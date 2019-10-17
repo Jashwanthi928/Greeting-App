8df0f26608e120d432f04e0199d7dcd8ab9e8fad
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-17 12:03:44 +0530

    settimeout

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ function clock_time() {
                 else if (17<=h && h<19) {document.getElementById('greet').innerHTML = "GOOD EVENING"}
                  else{document.getElementById('greet').innerHTML = "GOOD NIGHT"  
                 }
-            
+                function clock_time() {
+                    var today = new Date();
+                    var h= today.getHours();
+                   
+                  
+                    
+                    if(h<12) {document.getElementById('greet').innerHTML = "GOOD MORNING"}
+                        else if (12<=h && h<17) {document.getElementById('greet').innerHTML = "GOOD AFTERNOON"
+                        }
+                        else if (17<=h && h<19) {document.getElementById('greet').innerHTML = "GOOD EVENING"}
+                         else{document.getElementById('greet').innerHTML = "GOOD NIGHT"  
+                        }
+                    var t=setTimeout(clock_time);
+                } 
         } 
     } 
 } 
